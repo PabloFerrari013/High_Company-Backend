7e38b4f2731751070d68dbd521cc4bdf324faa4a
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2023-03-07 10:38:36 -0300

    editing connection with redis

--- build/src/lib/redis.js
+++ build/src/lib/redis.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.redisClient = void 0;
 var Redis = require('ioredis');
 exports.redisClient = new Redis({
-    url: process.env.REDIS_TLS_URL
+    url: 'rediss://:p04624877644dd85ce9e7c4bf00d4fdc9e39b245b514217622510578809e8edda@ec2-44-207-143-113.compute-1.amazonaws.com:8720'
 });
 //# sourceMappingURL=redis.js.map
