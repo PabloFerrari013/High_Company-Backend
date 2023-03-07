194beca96d16754314870292b9d92ce3e6ee1ddc
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2023-03-07 11:02:41 -0300

    changing redis settings

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,10 @@
 const Redis = require('ioredis')
 
 export const redisClient = new Redis(
-  'redis://:p04624877644dd85ce9e7c4bf00d4fdc9e39b245b514217622510578809e8edda@ec2-44-207-143-113.compute-1.amazonaws.com:8720'
+  'redis://:p04624877644dd85ce9e7c4bf00d4fdc9e39b245b514217622510578809e8edda@ec2-44-207-143-113.compute-1.amazonaws.com:8720',
+  {
+    tls: {
+      rejectUnauthorized: false
+    }
+  }
 )
