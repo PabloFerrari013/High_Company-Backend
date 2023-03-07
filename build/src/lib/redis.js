21b1deabd06a260d24be467d371348a4dfb425df
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2023-03-07 10:57:13 -0300

    changing redis settings

--- build/src/lib/redis.js
+++ build/src/lib/redis.js
@@ -2,10 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.redisClient = void 0;
 var Redis = require('ioredis');
-exports.redisClient = new Redis({
-    url: 'rediss://:p04624877644dd85ce9e7c4bf00d4fdc9e39b245b514217622510578809e8edda@ec2-44-207-143-113.compute-1.amazonaws.com:8720',
-    tls: {
-        rejectUnauthorized: false
-    }
-});
+exports.redisClient = new Redis('redis://:p04624877644dd85ce9e7c4bf00d4fdc9e39b245b514217622510578809e8edda@ec2-44-207-143-113.compute-1.amazonaws.com:8720');
 //# sourceMappingURL=redis.js.map
